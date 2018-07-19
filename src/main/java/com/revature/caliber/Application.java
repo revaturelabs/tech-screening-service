e5b64f319a75f9ae41a66f84d7ea560769c2f292
jspahlin <jspahlin@kent.edu>
2018-07-19 11:44:48 -0400

    Remove extra readme.md files, remove extra info in toplevel readme, remove ojdbc test from Application.java

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws ClassNotFoundException {
-		System.out.println( Class.forName("oracle.jdbc.driver.OracleDriver"));
 		SpringApplication.run(Application.class, args);
 	}
 	
