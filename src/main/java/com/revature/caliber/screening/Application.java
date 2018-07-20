c12853c8984c0b58a5f657a20ea1383a0d0506d3
Aware14 <aaronware14@gmail.com>
2018-07-20 10:32:28 -0400

    Updating package directory, adding repositories

---
+++ src/main/java/com/revature/caliber/screening/Application.java
@@ -0,0 +1,12 @@
+package com.revature.caliber.screening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) throws ClassNotFoundException {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
