ef2a2b604f1c66e6b3eef17edaa48bb0427eab5b
j-lasorella <jakob.lasorella@live.com>
2018-07-20 14:27:54 -0400

    all beans are in

--- src/main/java/com/revature/caliber/services/ScreeningService.java
+++ src/main/java/com/revature/caliber/services/ScreeningService.java
@@ -1,5 +1,11 @@
 package com.revature.caliber.services;
 
-public class ScreeningService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScreeningService {
+	
 }
