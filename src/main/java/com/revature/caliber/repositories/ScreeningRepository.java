91c6ae628bd4a5622404bcad17de8894ca2f1986
j-lasorella <jakob.lasorella@live.com>
2018-07-23 10:25:00 -0400

    Prep for pull

--- src/main/java/com/revature/caliber/repositories/ScreeningRepository.java
+++ src/main/java/com/revature/caliber/repositories/ScreeningRepository.java
@@ -1,9 +1,6 @@
 package com.revature.caliber.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.revature.caliber.beans.SimpleScreening;
