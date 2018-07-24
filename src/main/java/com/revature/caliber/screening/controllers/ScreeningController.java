eb0a4e210b84eca504c5b110699792ad27f1ec4b
j-lasorella <jakob.lasorella@live.com>
2018-07-24 09:18:40 -0400

    commit before pull

--- src/main/java/com/revature/caliber/screening/controllers/ScreeningController.java
+++ src/main/java/com/revature/caliber/screening/controllers/ScreeningController.java
@@ -1,20 +1,16 @@
 package com.revature.caliber.screening.controllers;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.caliber.beans.Screening;
-import com.revature.caliber.beans.SimpleScreening;
 import com.revature.caliber.screening.data.ScreeningRepository;
 
 @RestController
