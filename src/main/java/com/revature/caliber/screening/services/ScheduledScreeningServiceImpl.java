c2a66d5a9e18085bade5bba525da077f36e7bdba
Thomas Santillan <tsantillan93@gmail.com>
2018-07-26 09:44:44 -0400

    rebuilt to incorperate 1805-Richard changes

---
+++ src/main/java/com/revature/caliber/screening/services/ScheduledScreeningServiceImpl.java
@@ -0,0 +1,37 @@
+package com.revature.caliber.screening.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.caliber.beans.ScheduledScreening;
+import com.revature.caliber.screening.data.ScheduledScreeningRepository;
+
+@Service
+public class ScheduledScreeningServiceImpl implements ScheduledScreeningService {
+
+	@Autowired 
+	private ScheduledScreeningRepository scheduledScreeningRepository;
+	
+	@Override
+	public List<ScheduledScreening> findByStatus(String status) {
+		List<ScheduledScreening> simpleScheduledScreenings = null;
+		if ("PENDING".equals(status) || "SCREENED".equals(status)) {
+			simpleScheduledScreenings = scheduledScreeningRepository.findByStatus(status);
+		}
+		return simpleScheduledScreenings;
+	}
+
+	@Override
+	public void updateStatus(String screened, ScheduledScreening scheduledScreening) throws IllegalArgumentException {
+		if ("PENDING".equals(screened) || "SCREENED".equals(screened)) {
+			scheduledScreening.setStatus(screened);
+			scheduledScreeningRepository.save(scheduledScreening);
+		} else {
+			throw new IllegalArgumentException("Status must be 'PENDING' or 'SCREENED'");
+		}
+	}
+	
+
+}
