d19cacc812a1d911481d5aa30d34b71cfab203a4
Thomas Santillan <tsantillan93@gmail.com>
2018-07-27 16:05:00 -0400

    one more time

--- src/main/java/com/revature/caliber/screening/controllers/ScreeningController.java
+++ src/main/java/com/revature/caliber/screening/controllers/ScreeningController.java
@@ -6,22 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.caliber.beans.ScheduledScreening;
 import com.revature.caliber.beans.Screening;
 import com.revature.caliber.beans.SoftSkillViolation;
 import com.revature.caliber.beans.ViolationType;
+import com.revature.caliber.screening.services.ScheduledScreeningService;
 import com.revature.caliber.screening.services.ScreeningService;
 import com.revature.caliber.screening.services.SoftSkillViolationService;
 import com.revature.caliber.screening.services.ViolationTypeService;
 import com.revature.caliber.screening.wrappers.ViolationFlagWrapper;
 
 @RestController
+@CrossOrigin
 public class ScreeningController {
+	
+	@Autowired
+	ScheduledScreeningService scheduledScreeningService;
 	
 	@Autowired
 	ScreeningService screeningService;
@@ -81,6 +88,25 @@ public class ScreeningController {
 		SoftSkillViolation ssv = new SoftSkillViolation(screening, vt, violationFlag.softSkillComment, violationFlag.violationTime);
 		softSkillViolationService.save(ssv);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	/**
+	 * Get screenings based on the status provided.
+	 * 
+	 * @param status - A string notifying whether the screening is pending or complete.
+	 * @return - List of SimpleScreening objects corresponding to status.
+	 */
+	@RequestMapping(value="/screening/scheduled", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<ScheduledScreening>> getAllScheduledScreenings(){
+		List<ScheduledScreening> scheduledScreenings = scheduledScreeningService.findByStatus("PENDING");
+		
+		return new ResponseEntity<>(scheduledScreenings, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/screening/start", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Integer> createScreening(@RequestBody Screening screening){
+		Screening i = screeningService.setPending(screening);
+		return new ResponseEntity<>(i.getScreeningId(),HttpStatus.OK);
 	}
 	
 }
