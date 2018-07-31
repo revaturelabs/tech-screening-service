edb98a494b48efacc578857e8846bf6537e9c63c
Aware14 <aaronware14@gmail.com>
2018-07-31 09:32:23 -0400

    Fixing vulnerabilities in ViolationFlagWrapper

--- src/main/java/com/revature/caliber/screening/wrappers/ViolationFlagWrapper.java
+++ src/main/java/com/revature/caliber/screening/wrappers/ViolationFlagWrapper.java
@@ -6,8 +6,34 @@ import java.util.Date;
  * Wrapper class
  */
 public class ViolationFlagWrapper {
-	public Integer violationTypeId;
-	public String softSkillComment;
-	public Date violationTime;
-	public Integer screeningId;
+	private Integer violationTypeId;
+	private String softSkillComment;
+	private Date violationTime;
+	private Integer screeningId;
+	
+	public Integer getViolationTypeId() {
+		return violationTypeId;
+	}
+	public void setViolationTypeId(Integer violationTypeId) {
+		this.violationTypeId = violationTypeId;
+	}
+	public String getSoftSkillComment() {
+		return softSkillComment;
+	}
+	public void setSoftSkillComment(String softSkillComment) {
+		this.softSkillComment = softSkillComment;
+	}
+	public Date getViolationTime() {
+		return violationTime;
+	}
+	public void setViolationTime(Date violationTime) {
+		this.violationTime = violationTime;
+	}
+	public Integer getScreeningId() {
+		return screeningId;
+	}
+	public void setScreeningId(Integer screeningId) {
+		this.screeningId = screeningId;
+	}
+	
 }
