b9651e79e22349b7f9f8a8f86ef7da9c5a566ad7
Thomas Santillan <tsantillan93@gmail.com>
2018-07-31 23:35:17 -0400

    clean up

--- src/test/java/com/revature/caliber/screening/services/ScreeningServiceTest.java
+++ src/test/java/com/revature/caliber/screening/services/ScreeningServiceTest.java
@@ -1,39 +1,40 @@
 package com.revature.caliber.screening.services;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.revature.caliber.beans.Candidate;
 import com.revature.caliber.beans.Screening;
 import com.revature.caliber.screening.Application;
 import com.revature.caliber.screening.data.ScreeningRepository;
 import com.revature.caliber.screening.wrappers.StartingWrapper;
 
+/**
+ * Scheduled Screening Repository Testing class
+ * 
+ * @author Thomas Santillan | 1805-WVU-JULY27 | 
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes=Application.class)
 public class ScreeningServiceTest {
 	
-	 // @author Jakob LaSorella | 1805-WVU-MAY29 | Richard Orr
+	@Autowired
+	ScreeningService ss;
 	
 	@Autowired
-	ScreeningService screeningService;
-	
-	@Autowired
-	ScreeningRepository screeningRepo;
+	ScreeningRepository sr;
 		
 	@Test
 	public final void testServiceSave() {
 		StartingWrapper sw = new StartingWrapper();
 		sw.setScheduledScreeningId(1);
 		sw.setTrainerId(1);
-		Screening screening = screeningService.setPending(sw);
-		assertTrue(screening.getScreeningId() != 0);
-		assertEquals("PENDING", screening.getStatus());
+		Screening screening = ss.setPending(sw);
+		Assert.assertTrue(screening.getScreeningId() != 0);
+		Assert.assertEquals("PENDING", screening.getStatus());
 	}
 }
