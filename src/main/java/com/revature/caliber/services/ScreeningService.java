1784ce4ae231a03c304d6ee6ca655b8a9b65e8f1
Jeremy Straus <jstraws@verizon.net>
2018-09-26 16:42:34 -0400

    Refactor ScreeningService, removing wrapper classes, writing and cleaning Javadocs, and using Optionals.

--- src/main/java/com/revature/caliber/services/ScreeningService.java
+++ src/main/java/com/revature/caliber/services/ScreeningService.java
@@ -4,16 +4,50 @@ import com.revature.caliber.beans.Screening;
 import com.revature.caliber.wrappers.EndingWrapper;
 import com.revature.caliber.wrappers.StartingWrapper;
 
-
+/**
+ * The service for the Screening part of the Screening service.
+ *
+ * @author Jakob LaSorella | 1805-WVU-MAY29 | Richard Orr
+ * @author Jeremy Straus | 1807-QC | Emily Higgins
+ */
 public interface ScreeningService {
-	
-	// @author Jakob LaSorella | 1805-WVU-MAY29 | Richard Orr
-	
+	/**
+	 * Get a screening by the Id
+	 *
+	 * @param screeningId Id to filter screenings by
+	 * @return The screening with the matching Id
+	 */
+	Screening getScreening(int screeningId);
 
-	public Screening getScreening(int id);
-	public void updateAboutMeComment(String aboutMeComment, Integer screeningId);
-	public void updateGeneralComment(String generalComment, Integer screeningId);
-	public void endScreening(EndingWrapper screeningInfo);
-	public Screening setPending(StartingWrapper screeningInfo);
+	/**
+	 * Update aboutMeComment by ScreeningId
+	 *
+	 * @param aboutMeComment - the comment to be persisted to the database.
+	 * @param screeningId    - the unique id of a screening
+	 */
+	void updateAboutMeComment(String aboutMeComment, int screeningId);
+
+	/**
+	 * Update GeneralComment By ScreeningId
+	 *
+	 * @param generalComment - the comment to be persisted to the database
+	 * @param screeningId    - the unique id of a screening
+	 */
+	void updateGeneralComment(String generalComment, int screeningId);
+
+	/**
+	 * Update info after a Screening ends
+	 *
+	 * @param screeningInfo - Screening info to be updated after a Screening ends
+	 */
+	void endScreening(EndingWrapper screeningInfo);
+
+	/**
+	 * Set a screening as pending
+	 *
+	 * @param screeningInfo Screening info to be updated after a screening ends
+	 * @return Returns the sceening being updated
+	 */
+	Screening setPending(StartingWrapper screeningInfo);
 }
 
