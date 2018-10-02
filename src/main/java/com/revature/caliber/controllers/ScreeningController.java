70976635769514656fa47684c92b45ca0ea07ea5
Jeremy Straus <jstraws@verizon.net>
2018-10-02 13:35:53 -0400

    Annotate ScreeningController.

--- src/main/java/com/revature/caliber/controllers/ScreeningController.java
+++ src/main/java/com/revature/caliber/controllers/ScreeningController.java
@@ -7,6 +7,10 @@ import com.revature.caliber.beans.SoftSkillViolation;
 import com.revature.caliber.services.ScheduledScreeningService;
 import com.revature.caliber.services.ScreeningService;
 import com.revature.caliber.services.SoftSkillViolationService;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,6 +26,7 @@ import java.util.List;
 @CrossOrigin
 @RestController
 @RequestMapping("/screening")
+@ApiModel(value = "Screening Controller", description = "A REST controller to handle HTTP requests that deal with Screenings and ScheduledScreenings")
 public class ScreeningController {
 
 	@Autowired
@@ -39,6 +44,8 @@ public class ScreeningController {
 	 * @param screeningId - the unique id of a Screening
 	 * @return List of SoftSkillViolation objects
 	 */
+	@ApiOperation(value = "Get all the violations for a Screening", response = SoftSkillViolation.class, responseContainer = "List")
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Retrieved all violations")})
 	@RequestMapping(value = "/{id}/violations", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<SoftSkillViolation>> softSkillViolationByScreeningID(@PathVariable(value = "id") int screeningId) {
 		List<SoftSkillViolation> ssv = softSkillViolationService.getAllByScreeningId(screeningId);
@@ -51,6 +58,8 @@ public class ScreeningController {
 	 *
 	 * @return - List of ScheduledScreening objects corresponding to status.
 	 */
+	@ApiOperation(value = "Get all pending Scheduled Screenings", response = ScheduledScreening.class, responseContainer = "List")
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Retrieved all Scheduled Screenings")})
 	@RequestMapping(value = "/scheduled", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<ScheduledScreening>> getAllScheduledScreenings() {
 		List<ScheduledScreening> scheduledScreenings = scheduledScreeningService.findByStatus(ScheduledStatus.PENDING);
@@ -64,6 +73,11 @@ public class ScreeningController {
 	 * @param screening Screening to
 	 * @return New screening object
 	 */
+	@ApiOperation(value = "Add a new Screening", response = Screening.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Screening added"),
+			@ApiResponse(code = 403, message = "Bad request, screening not added")
+	})
 	@PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Screening> createScreening(@Valid @RequestBody Screening screening) {
 		Screening newScreen = screeningService.createScreening(screening);
@@ -80,6 +94,11 @@ public class ScreeningController {
 	 * @param screening Screening to update
 	 * @return Updated screening object
 	 */
+	@ApiOperation(value = "Update a screening", response = Screening.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Screening updated"),
+			@ApiResponse(code = 403, message = "Bad request, screening not updated")
+	})
 	@PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Screening> updateScreening(@Valid @RequestBody Screening screening) {
 		Screening updateScreening = screeningService.updateScreening(screening);
