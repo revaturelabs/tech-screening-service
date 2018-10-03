4d039e811b29b329b0c1582b21c5d1777b567ffe
Jeremy Straus <jstraws@verizon.net>
2018-10-03 18:19:07 -0400

    100% Method and Line coverage

--- src/test/java/com/revature/caliber/services/ScreeningServiceImplTest.java
+++ src/test/java/com/revature/caliber/services/ScreeningServiceImplTest.java
@@ -1,10 +1,7 @@
 package com.revature.caliber.services;
-import com.revature.caliber.beans.*;
-
-
 
 import com.revature.caliber.Application;
-
+import com.revature.caliber.beans.Screening;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +18,7 @@ import static org.junit.Assert.*;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @AutoConfigureTestDatabase
 public class ScreeningServiceImplTest {
-	
+
 	@Autowired
 	ScreeningServiceImpl screeningService;
 
@@ -36,9 +33,16 @@ public class ScreeningServiceImplTest {
 		Screening sc = new Screening(null, 3, 0, null, "Test", "Test1", null, null, null, null, null);
 		assertNotNull(screeningService.createScreening(sc));
 	}
+
 	@Test
 	public void testCreateNullScreening() {
 		Screening sc = null;
+		assertNull(screeningService.createScreening(sc));
+	}
+
+	@Test
+	public void testCreateEmptyScreening() {
+		Screening sc = new Screening();
 		assertNull(screeningService.createScreening(sc));
 	}
 
@@ -50,7 +54,17 @@ public class ScreeningServiceImplTest {
 		screeningService.updateScreening(screening);
 		double actual = screeningService.getScreening(4322).getCompositeScore();
 		assertEquals(expected, actual, 0);
-	
 	}
 
+	@Test
+	public void testUpdateNullScreening() {
+		Screening screening = null;
+		assertNull(screeningService.updateScreening(screening));
+	}
+
+	@Test
+	public void testUpdateEmptyScreening() {
+		Screening screening = new Screening();
+		assertNull(screeningService.updateScreening(screening));
+	}
 }
