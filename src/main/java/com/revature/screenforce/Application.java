84ab7feacaefa4ca842c2358ff92638944d41e6e
TuckerBelton <tuckerbelton@yahoo.com>
2019-05-24 14:45:54 -0400

    Advice Inserted

--- src/main/java/com/revature/screenforce/Application.java
+++ src/main/java/com/revature/screenforce/Application.java
@@ -1,10 +1,16 @@
 package com.revature.screenforce;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -14,4 +20,15 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+	@Bean
+	public Docket swag() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.revature.screenforce"))
+				.paths(regex("/.*"))
+				.build();
+	}
+
 }
